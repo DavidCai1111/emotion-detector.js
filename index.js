824559da058e8d6ad57d4e67d3ee3547de704993
David Cai <davidcai1993@yahoo.com>
2018-07-21 15:14:16 +0800

    feat: save image

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 'use strict'
 require('@tensorflow/tfjs-node')
 const tf = require('@tensorflow/tfjs')
-const jimp = require('jimp')
+const imageUtil = require('./lib/image')
 
+;(async function () {
+  const rgbImage = await imageUtil.loadImage('./images/faces.jpg', false)
+  const grayImage = await imageUtil.loadImage('./images/faces.jpg', true)
+
+  rgbImage.print()
+  grayImage.print()
+
+  await imageUtil.saveImage('./test1.jpg', grayImage)
+  await imageUtil.saveImage('./test2.jpg', rgbImage)
+})(console.error)
