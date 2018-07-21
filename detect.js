90d85cccaa5e3b091e2d928ec2f809c7f7afeadf
David Cai <davidcai1993@yahoo.com>
2018-07-21 19:56:32 +0800

    feat: change face threshold

--- detect.js
+++ detect.js
@@ -15,20 +15,20 @@ const EMOTION_MODEL_PATH = `file://${__dirname}/models/fer2013/model.json`
     emotionModel.feedInputShapes[0][2]
   ]
 
-  let imageRGB = await imageUtil.loadImage('./images/faces.jpg', false)
-  let imageGray = await imageUtil.loadImage('./images/faces.jpg', true)
+  let imageRGB = await imageUtil.loadImage('./images/ronaldo.JPG', false)
+  let imageGray = await imageUtil.loadImage('./images/ronaldo.JPG', true)
 
   const faces = await faceUtil.getFaces(imageGray)
 
   for (const face of faces) {
     const x = cv.Point2(face.x, face.y)
     const y = cv.Point2(face.x + face.width, face.y + face.height)
-    imageRGB.drawRectangle(x, y)
+    imageRGB.drawRectangle(x, y, new cv.Vec3(255, 255, 255))
 
     let faceImage = await imageRGB.getRegion(face)
     let tensor = await faceUtil.preprocessToTensor(faceImage, inputShape)
 
-    imageRGB.putText(await faceUtil.inferEmotion(tensor, emotionModel), x, 0, 1)
+    imageRGB.putText(await faceUtil.inferEmotion(tensor, emotionModel), x, 0, 1, new cv.Vec3(255, 255, 255))
   }
 
   await cv.imwriteAsync('./test.jpg', imageRGB)
